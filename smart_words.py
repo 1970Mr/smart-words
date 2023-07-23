a3ae82c8117f8a6d035eb34e7b6a820d1f4073d3
github-1970 <github1970@gmail.com>
2023-07-23 14:39:23 +0330

    Save message history for each file

--- smart_words.py
+++ smart_words.py
@@ -7,20 +7,23 @@ import argparse
 openai.api_key = "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"
 model_name = "gpt-3.5-turbo-16k"
 max_tokens = 15000
-temperature = 0.6
+temperature = 0.7
 
-def generate_article(prompt, min_tokens=None):
+def generate_article(prompt, message_history=None, min_tokens=None):
     sections = []
     response = None
     total_tokens = 0
 
     while not response or (min_tokens and total_tokens < min_tokens):
+        if message_history:
+            messages = message_history.copy()
+            messages.append({"role": "user", "content": prompt})
+        else:
+            messages = [{"role": "user", "content": prompt}]
+
         response = openai.ChatCompletion.create(
             model=model_name,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt},
-            ],
+            messages=messages,
             max_tokens=max_tokens - total_tokens,
             temperature=temperature,
         )
@@ -44,15 +47,23 @@ def process_requests(file_path, min_tokens=None):
     with open(file_path, "r", encoding="utf-8") as file:
         sections = file.read().split("[SECTION]")
 
+    message_history = None
+
     for idx, section in enumerate(sections[1:], start=1):
         prompt = section.strip()
-        response = generate_article(prompt, min_tokens)
+        response = generate_article(prompt, message_history, min_tokens)
 
         if not os.path.exists("outputs"):
             os.makedirs("outputs")
         response_filename = f"outputs/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
         with open(response_filename, "w", encoding="utf-8") as response_file:
             response_file.write(response)
+
+        # Update message history with the current response
+        if message_history:
+            message_history.append({"role": "assistant", "content": response})
+        else:
+            message_history = [{"role": "assistant", "content": response}]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate articles using ChatGPT.")
