ac74d6cfc4bc158eafc2102eb785e0b2fc848012
1970Mr <mrepoch1970@gmail.com>
2023-07-23 11:50:45 +0330

    Update gitignore

--- smart_words.py
+++ smart_words.py
@@ -3,15 +3,15 @@ import os
 import sys
 import time
 
-# تنظیم متغیرهای مربوط به ChatGPT API
+# Set the variables related to the ChatGPT API
 openai.api_key = "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"
 model_name = "gpt-3.5-turbo-16k"
 
 def generate_article(prompt):
     full_response = ""
-    max_tokens = 15000  # تنظیم حداکثر تعداد توکن‌ها برای هر بار تولید
+    max_tokens = 15000  # Set the maximum number of tokens for each generation
 
-    while len(full_response) < 3000:  # تا زمانی که کل متن پاسخ کمتر از 700 کلمه باشد، ادامه دهید
+    while len(full_response) < 3000:  # Continue until the total response text is less than 3000 tokens
         response = openai.ChatCompletion.create(
             model=model_name,
             messages=[
@@ -23,12 +23,12 @@ def generate_article(prompt):
         )
         generated_text = response.choices[0].message["content"].strip()
         full_response += generated_text
-        prompt = generated_text  # استفاده از پاسخ تولید شده به عنوان prompt برای ادامه
+        prompt = generated_text  # Use the generated response as the prompt for the next iteration
         time.sleep(10)
 
     return full_response
 
-# تابع برای مدیریت تاریخچه و ذخیره پاسخ‌ها
+# Function to manage history and save responses
 def process_requests(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         sections = file.read().split("sectionRaS")
@@ -37,15 +37,15 @@ def process_requests(file_path):
         prompt = section.strip()
         response = generate_article(prompt)
 
-        # ذخیره پاسخ در پوشه response با نام همان فایل درخواست
-        if not os.path.exists("outputs"):
-            os.makedirs("outputs")
-        response_filename = f"outputs/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
+        # Save the response in the response directory with the same request file name
+        if not os.path.exists("response"):
+            os.makedirs("response")
+        response_filename = f"response/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
         with open(response_filename, "w", encoding="utf-8") as response_file:
             response_file.write(response)
 
 if __name__ == "__main__":
-    # اسکن فایل‌ها در پوشه templates و اجرای مقاله‌نویسی برای هر فایل
+    # Scan files in the templates directory and generate articles for each file
     templates_dir = "templates"
     for filename in os.listdir(templates_dir):
         file_path = os.path.join(templates_dir, filename)
