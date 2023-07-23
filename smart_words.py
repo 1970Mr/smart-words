0a3210609a68d58785399c1de12ccb123de2afe1
github-1970 <github1970@gmail.com>
2023-07-23 17:05:06 +0330

    Add color for any messages

--- smart_words.py
+++ smart_words.py
@@ -4,12 +4,14 @@ import sys
 import argparse
 import time
 from datetime import datetime
+from colorama import Fore, Style
 
 # Set the variables related to the ChatGPT API
 openai.api_key = "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"
 model_name = "gpt-3.5-turbo-16k"
 max_tokens = 15000
 temperature = 0.6
+
 
 def generate_article(prompt, message_history=None, min_tokens=None):
     sections = []
@@ -35,9 +37,9 @@ def generate_article(prompt, message_history=None, min_tokens=None):
         end_time = time.time()
         duration = end_time - start_time
 
-        message = response['choices'][0]['message']
-        generated_text = message['content'].strip()
-        finish_reason = message['role']
+        message = response["choices"][0]["message"]
+        generated_text = message["content"].strip()
+        finish_reason = message["role"]
 
         sections.append(generated_text)
         prompt = generated_text
@@ -49,11 +51,25 @@ def generate_article(prompt, message_history=None, min_tokens=None):
             break
 
     # Print details for each section
-    print(f"Total characters generated: {len(generated_text)}")
-    print(f"Total words generated: {len(generated_text.split())}")
-    print(f"Section Generation Time: {duration:.2f} seconds\n")
+    print(
+        Fore.GREEN
+        + f"Total characters generated: {len(generated_text)}"
+        + Style.RESET_ALL
+    )
+    print(
+        Fore.GREEN
+        + f"Total words generated: {len(generated_text.split())}"
+        + Style.RESET_ALL
+    )
+    print(
+        Fore.GREEN
+        + f"Section Generation Time: {duration:.2f} seconds"
+        + Style.RESET_ALL
+        + "\n"
+    )
 
     return "".join(sections)
+
 
 def process_requests(file_path, min_tokens=None, save_message_history=False):
     with open(file_path, "r", encoding="utf-8") as file:
@@ -63,7 +79,11 @@ def process_requests(file_path, min_tokens=None, save_message_history=False):
     total_sections = len(sections) - 1
 
     for idx, section in enumerate(sections[1:], start=1):
-        print(f"Current Section: {idx}/{total_sections}")
+        print(
+            Fore.CYAN
+            + f"Current Section: {Fore.LIGHTBLACK_EX}{idx}{Style.RESET_ALL}{Fore.CYAN}/{total_sections}"
+            + Style.RESET_ALL
+        )
         prompt = section.strip()
         if save_message_history:
             response = generate_article(prompt, message_history, min_tokens)
@@ -72,7 +92,9 @@ def process_requests(file_path, min_tokens=None, save_message_history=False):
 
         if not os.path.exists("outputs"):
             os.makedirs("outputs")
-        response_filename = f"outputs/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
+        response_filename = (
+            f"outputs/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
+        )
         with open(response_filename, "w", encoding="utf-8") as response_file:
             response_file.write(response)
 
@@ -84,15 +106,18 @@ def process_requests(file_path, min_tokens=None, save_message_history=False):
                 message_history = [{"role": "assistant", "content": response}]
 
             # Add a newline after each message history
-            message_history[-1]['content'] += "\n"
-        
+            message_history[-1]["content"] += "\n"
+
 
 if __name__ == "__main__":
     start_time = datetime.now()
 
     parser = argparse.ArgumentParser(description="Generate articles using ChatGPT.")
     parser.add_argument(
-        "--min_tokens", type=int, default=None, help="Minimum number of tokens for each section."
+        "--min_tokens",
+        type=int,
+        default=None,
+        help="Minimum number of tokens for each section.",
     )
     parser.add_argument(
         "--save_message_history",
@@ -107,9 +132,9 @@ if __name__ == "__main__":
         file_path = os.path.join(templates_dir, filename)
         if os.path.isfile(file_path):
             process_requests(file_path, args.min_tokens, args.save_message_history)
-    
+
     end_time = datetime.now()
     total_time = end_time - start_time
     # Convert total_time to the desired format
     total_time_str = str(total_time)[:-4]
-    print(f"Total Execution Time: {total_time_str}")
+    print(Fore.MAGENTA + f"Total Execution Time: {total_time_str}" + Style.RESET_ALL)
