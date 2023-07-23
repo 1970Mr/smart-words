569ad4b4591009f2b24aa9cf73d71b41753b0ba3
github-1970 <github1970@gmail.com>
2023-07-23 11:48:47 +0330

    Completion of SmartWords by producing at least three thousand words

--- smart_words.py
+++ smart_words.py
@@ -1,39 +1,53 @@
-import requests
+import openai
+import os
+import sys
+import time
 
-def get_chatgpt_response(message):
-    api_url = "https://api.openai.com/v1/chat/completions"
-    api_key = "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"
+# تنظیم متغیرهای مربوط به ChatGPT API
+openai.api_key = "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"
+model_name = "gpt-3.5-turbo-16k"
 
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    }
+def generate_article(prompt):
+    full_response = ""
+    max_tokens = 15000  # تنظیم حداکثر تعداد توکن‌ها برای هر بار تولید
 
-    payload = {
-        # "model": "gpt-3.5-turbo",
-        "model": "gpt-3.5-turbo-16k",
-        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": message}]
-    }
+    while len(full_response) < 3000:  # تا زمانی که کل متن پاسخ کمتر از 700 کلمه باشد، ادامه دهید
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=max_tokens,
+            temperature=0.7,
+        )
+        generated_text = response.choices[0].message["content"].strip()
+        full_response += generated_text
+        prompt = generated_text  # استفاده از پاسخ تولید شده به عنوان prompt برای ادامه
+        time.sleep(10)
 
-    response = requests.post(api_url, json=payload, headers=headers)
+    return full_response
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["choices"][0]["message"]["content"]
-    else:
-        return f"Error: {response.status_code}, {response.text}"
+# تابع برای مدیریت تاریخچه و ذخیره پاسخ‌ها
+def process_requests(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        sections = file.read().split("sectionRaS")
 
-def main():
-    print("Hello! I am ChatGPT. To end the conversation, type 'end'.")
+    for idx, section in enumerate(sections[1:], start=1):
+        prompt = section.strip()
+        response = generate_article(prompt)
 
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == "end":
-            print("Conversation ended.")
-            break
-
-        response = get_chatgpt_response(user_input)
-        print("ChatGPT:", response)
+        # ذخیره پاسخ در پوشه response با نام همان فایل درخواست
+        if not os.path.exists("outputs"):
+            os.makedirs("outputs")
+        response_filename = f"outputs/{os.path.splitext(os.path.basename(file_path))[0]}_{idx}.txt"
+        with open(response_filename, "w", encoding="utf-8") as response_file:
+            response_file.write(response)
 
 if __name__ == "__main__":
-    main()
+    # اسکن فایل‌ها در پوشه templates و اجرای مقاله‌نویسی برای هر فایل
+    templates_dir = "templates"
+    for filename in os.listdir(templates_dir):
+        file_path = os.path.join(templates_dir, filename)
+        if os.path.isfile(file_path):
+            process_requests(file_path)
